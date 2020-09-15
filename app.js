2085b84cfe342ec0e45d683a29a947e5f489fd17
James Cameron-Mowat <jcm802@gmail.com>
2020-09-15 14:21:32 +0100

    adjusted server to heroku dynamic port

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get("/home", (req, res) => {
 // ======
 // SERVER
 // ======
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server on port 3000 started...");
 });
 
